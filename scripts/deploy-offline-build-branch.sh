a3adb99fed6d7bfc72607c5009d82f559fd56b23
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-10-01 23:59:32 +0200

    authentication with ssh agent
    https://travis-ci.org/CoderDojoPotsdam/intro#L1098
    https://github.com/CoderDojoPotsdam/software/blob/master/linux-update/update_git_repository.sh#L16

--- scripts/deploy-offline-build-branch.sh
+++ scripts/deploy-offline-build-branch.sh
@@ -13,9 +13,6 @@ if ! [ -n "$OFFLINE_BUILD_FOLDER" ]; then
   exit 1
 fi
 
-mkdir -p ~/.ssh
-echo "$id_rsa" > ~/.ssh/id_rsa
-
 echo "Create a temporary folder for checking out $BRANCH."
 mkdir -p temp
 cp -r .git temp
@@ -34,6 +31,7 @@ rm -r temp
   echo "Commit offline version into branch \"$BRANCH\""
   git add --all .
   git commit -am"Automated build $TRAVIS_BUILD_NUMBER"
-  git push --set-upstream origin "$BRANCH"
+  echo "$id_rsa" > id_rsa
+  ssh-agent bash -c "ssh-add id_rsa; git push --set-upstream origin \"$BRANCH\""
   rm -r .git
 )
