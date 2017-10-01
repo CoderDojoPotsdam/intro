4f5e8aa17e19ddc60c551418209fc5abe73306de
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-10-01 23:06:10 +0200

    add deploy script

---
+++ scripts/deploy-offline-build-branch.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+BRANCH="offline-build"
+
+if ! [ -n "$id_rsa" ]; then
+  echo "You should set the variable id_rsa to contain a valid ssh deploy key to origin."
+  exit 1
+fi
+if ! [ -n "$OFFLINE_BUILD_FOLDER" ]; then
+  echo "You should set the variable OFFLINE_BUILD_FOLDER to contain the built version of the offline tutorial."
+  exit 1
+fi
+
+mkdir -p ~/.ssh
+echo "$id_rsa" > ~/.ssh/id_rsa
+
+echo "Create a temporary folder for checking out $BRANCH."
+mkdir -p temp
+cp -r .git temp
+(
+  set -e
+  cd temp
+  git checkout "$BRANCH"
+)
+mv temp/.git "$OFFLINE_BUILD_FOLDER"
+rm -r temp
+
+(
+  set -e
+  cd "$OFFLINE_BUILD_FOLDER"
+  echo "Commit offline version into branch \"$BRANCH\""
+  git add --all .
+  git commit -am"Automated build $TRAVIS_BUILD_NUMBER"
+  git push
+  rm -r .git
+)
