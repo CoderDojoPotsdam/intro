415ffcb427cf9e877fd4ec7a85f136711b1aaaea
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-10-09 23:53:16 +0200

    make pip installable

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 if sys.version_info < (3, 4):
     raise NotImplementedError("Sorry, you need at least Python 3.4 to use intro_offline_server.")
 
-__version__ = "1.0.0"
+__version__ = "1.0.2"
 __author__ = 'Nicco Kunzmann'
 
 HERE = os.path.abspath(os.path.dirname(__file__))
@@ -39,7 +39,7 @@ def read_requirements_file(file_name):
 required_packages = read_requirements_file("requirements.txt")
 
 setup(name='intro_offline_server',
-      version=intro_offline_server.__version__,
+      version=__version__,
       install_requires=required_packages,
       description='Server for intro material.',
       long_description=read_file_named("README.md"),
